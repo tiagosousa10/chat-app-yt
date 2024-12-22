2a0993fc5b27387caf6d2b530f7cbcff3397c7ea
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-22 20:14:15 +0000

    creating signup function

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,15 @@ import authRoutes from './routes/auth.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js'
 
 const app = express()
-
-dotenv.config()
 const PORT = 5000 ; //nao estou a usar o env , pois estava com conflitos de portas.
 
-app.get('/', (req, res) => {
+dotenv.config()
+
+/* app.get('/', (req, res) => {
   res.send('hello world')
-})
+}) */
+
+app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
