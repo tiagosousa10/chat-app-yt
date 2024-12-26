04064d927c1ca27f173aa10f43300fe6466a6207
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-26 17:40:47 +0000

    adding router dom and creating routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home.jsx'
-/* import Login from './pages/login/Login.jsx'
-import SignUp from './pages/signup/SignUp.jsx' */
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <SignUp /> */}
-      <Home/>
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>}  />
+     </Routes>
     </div>
   )
 }
