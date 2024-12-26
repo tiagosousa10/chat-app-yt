66dfce4bcbda39cd5c1cb3e1f69444dcbfc7c504
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-26 22:16:56 +0000

    creating logout hook

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import toast from "react-hot-toast"
+import { useAuthContext } from "../context/AuthContext"
 
 const useSignup = () => {
   const [loading,setLoading] = useState(false)
+  const { setAuthUser } = useAuthContext()
 
   const signup = async ({fullName,username,password,confirmPassword,gender}) => {
    const success = handleInputErrors({fullName,username,password,confirmPassword,gender})
@@ -22,8 +24,10 @@ const useSignup = () => {
     if(data.error) {
       throw new Error(data.error)
     }
-
     //localstorage 
+    localStorage.setItem('chat-user', JSON.stringify(data))
+    //context
+    setAuthUser(data)
 
   } catch (error) {
     toast.error(error.message)
