51f84e213586cd5e6957a0bf3534500f41484c34
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-26 16:49:31 +0000

    creating sidebar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home.jsx'
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home/>
     </div>
   )
 }
---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Conversation = () => {
+  return (
+    <>
+      <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer '>
+
+      </div>
+    </>
+  )
+}
+
+export default Conversation
