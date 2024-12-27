b0c1857ae3e954f7e4914a22f750714c547d4e6e
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-27 11:52:08 +0000

    adding emojis random function and fetching the conversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations'
+import { getRandomEmoji } from '../../utils/emojis'
 
 const Conversations = () => {
+const {loading, conversations} = useGetConversations()
+console.log( "Conversations: " , conversations)
+
   return (
     <div className='py-2 flex flex-col overflow-auto'> 
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversations, idx) => (
+        <Conversation  
+          key={conversations._id}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length -1 }
+        />
+      ))}
+     {loading  ? <span className='loading loading-spinner'></span> : null }
 
     </div>
   )
