b2fcacf2f608be1ee0cd3cb61d89a62e1f2c96e4
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 10:26:15 +0000

    creating send message hook

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -9,9 +9,10 @@ console.log( "Conversations: " , conversations)
 
   return (
     <div className='py-2 flex flex-col overflow-auto'> 
-      {conversations.map((conversations, idx) => (
+      {conversations.map((conversation, idx) => (
         <Conversation  
-          key={conversations._id}
+          key={conversation._id}
+          conversation={conversation}
           emoji={getRandomEmoji()}
           lastIdx={idx === conversations.length -1 }
         />
