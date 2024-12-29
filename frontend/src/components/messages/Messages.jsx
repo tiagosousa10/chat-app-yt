bf4a270b8f1f65fc6685141243f4e4095e8c1bde
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 11:43:47 +0000

    updating searchInput

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect , useRef } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import MessageSkeleton from '../skeletons/MessageSkeletons'
 
 const Messages = () => {
   const {messages, loading} = useGetMessages()
+  const lastMessageRef = useRef()
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({behavior : "smooth"})
+    }, 100)
+  }, [messages])
 
   return (
 		<div className='px-4 flex-1 overflow-auto'>
 			{!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
-					<div key={message._id} >
+					<div key={message._id} ref={lastMessageRef} >
 						<Message message={message} />
 					</div>
 				))}
