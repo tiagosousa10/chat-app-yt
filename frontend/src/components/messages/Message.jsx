e25dddea246b8998048ba35f462f08d29fcc2e9d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 17:12:26 +0000

    adding sound and shake message

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -4,13 +4,14 @@ import useConversation from '../../zustand/useConversation'
 import { extractTime } from '../../utils/extractTime';
 
 const Message = ({message}) => {
-  const { authUser } = useAuthContext();
+	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
 	const fromMe = message.senderId === authUser._id;
-  const formattedTime = extractTime(message.createdAt)
-  const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const formattedTime = extractTime(message.createdAt)
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+	const shakeClass = message.shouldShake ? 'shake' : '';
 
 
   return (
@@ -20,7 +21,7 @@ const Message = ({message}) => {
 					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
 			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import {useSocketContext} from '../context/SocketContext'
 import useConversation from '../zustand/useConversation'
 
+import notificationSound from '../assets/sounds/notification.mp3'
 
 const useListenMessages = () => {
   const {socket} = useSocketContext()
@@ -10,6 +11,9 @@ const useListenMessages = () => {
 
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
+      newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound)
+      sound.play();
       setMessages([...messages, newMessage])
     })
 
