857ee2e49b2ffc8dbd3f534928ff86456a076008
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 10:52:38 +0000

    creating skeleton component

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeletons'
 
 const Messages = () => {
+  const {messages, loading} = useGetMessages()
+  console.log("messages: ",messages)
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {loading && [...Array(3)].map((_,index) => <MessageSkeleton key={index} />) }
 
     </div>
   )
