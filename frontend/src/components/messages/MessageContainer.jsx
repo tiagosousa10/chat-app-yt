c7316cd0deabaf0a2c94fad3df1bf829eae14574
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 11:20:37 +0000

    accessing the messages i the dashboard

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react'
-import Messages from './Messages'
-import MessageInput from './MessageInput'
-import {TiMessages} from 'react-icons/ti'
-import useConversation from '../../zustand/useConversation'
-
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-   const { selectedConversation, setSelectedConversation} = useConversation()
+	const { selectedConversation, setSelectedConversation } = useConversation();
 
-   useEffect(() => {
-      return () => setSelectedConversation(null) //cleanup 
-   } , [setSelectedConversation])
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
 
-  return (
-    <div className='md:min-w-[450px] flex flex-col'>
-     { !selectedConversation ?  <NoChatSelected /> : (
-      <>
-      {/*   HEADER   */}
-         <div className='bg-slate-500 px-4 py-2 mb-2'>
-            <span className='label-text'>To:</span>{" "}
-            <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-         </div>
-
-         <Messages />
-         <MessageInput />     
-         </>
-       )
-      }
-      
-    </div>
-  )
-}
+	return (
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
+};
 export default MessageContainer;
 
 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -8,11 +8,21 @@ const Messages = () => {
   console.log("messages: ",messages)
 
   return (
-    <div className='px-4 flex-1 overflow-auto'>
-      {loading && [...Array(3)].map((_,index) => <MessageSkeleton key={index} />) }
+		<div className='px-4 flex-1 overflow-auto'>
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} >
+						<Message message={message} />
+					</div>
+				))}
 
-    </div>
-  )
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+		</div>
+	);
 }
 
 export default Messages
