0ead25a2d32f3e252565d76a16419c48ef4e1622
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-29 16:45:36 +0000

    online users at current time

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,9 +1,13 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from 'socket.io-client'
 
 
 export const SocketContext = createContext()
+
+export const useSocketContext = () => {
+   return useContext(SocketContext)
+}
 
 export const SocketContextProvider = ({children}) => {
 
@@ -13,23 +17,30 @@ export const SocketContextProvider = ({children}) => {
 
    useEffect(() => {
       if(authUser) {
-         const socket = io("http://localhost:5000")
+         const socket = io("http://localhost:5000", {
+            query: {
+               userId: authUser._id
+            }
+         })
          setSocket(socket)
 
+         socket.on('getOnlineUsers', (users) => { //show online users
+            setOnlineUsers(users)
+         })
          return () => socket.close() //clean up
-         
+
       } else {
          if(socket) {
             socket.close()
             setSocket(null)
          }
       }
-   }, [])
+   }, [authUser])
 
 
 
    return(
-      <SocketContext.Provider value={{}}>
+      <SocketContext.Provider value={{socket, onlineUsers}}>
          {children}
       </SocketContext.Provider>
    )
